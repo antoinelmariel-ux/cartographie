c9c2c9965221a2c76bf0b2cca65180066d3de6b4
antoinelmariel-ux <antoinel.mariel@gmail.com>
2025-09-14 14:28:46 +0200

    Allow running without local server

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-import { RiskManagementSystem, setRms, bindEvents, applyPatch } from './rms.js';
-
 document.addEventListener('DOMContentLoaded', () => {
     const rms = new RiskManagementSystem();
     setRms(rms);
