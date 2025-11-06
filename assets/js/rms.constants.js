1e1588eb5fa1d301b0b439fd0003f66fa7006d2a
antoinelmariel-ux <antoinel.mariel@gmail.com>
2025-11-06 10:00:52 +0100

    Update rms.constants.js

--- assets/js/rms.constants.js
+++ assets/js/rms.constants.js
@@ -22,19 +22,19 @@ const RISK_PROBABILITY_INFO = {
 const RISK_IMPACT_INFO = {
     1: {
         label: 'Faible',
-        text: "<p><strong>Impact financier</strong></p><ul><li>Sanction interne disciplinaire envers un collaborateur</li></ul><p><strong>Impact juridique / r&eacute;glementaire</strong></p><ul><li>Sanction interne disciplinaire envers un collaborateur</li></ul><p><strong>Impact r&eacute;putationnel</strong></p><ul><li>Impact nul, interne ou externe local (ex : Partenaire)</li><li>Atteinte limit&eacute;e &agrave; quelques jours</li></ul><p><strong>Impact op&eacute;rationnel</strong></p><ul><li>Peu ou pas de perturbations</li><li>Rallentissement des activit&eacute;s</li></ul>"
+        text: "<p><strong>Impact financier</strong></p><ul><li>&lt; 300K&euro;</li></ul><p><strong>Impact juridique / r&eacute;glementaire</strong></p><ul><li>Sanction interne disciplinaire envers un collaborateur</li></ul><p><strong>Impact r&eacute;putationnel</strong></p><ul><li>Impact nul, interne ou externe local (ex : Partenaire)</li><li>Atteinte limit&eacute;e &agrave; quelques jours</li></ul><p><strong>Impact op&eacute;rationnel</strong></p><ul><li>Peu ou pas de perturbations</li><li>Rallentissement des activit&eacute;s</li></ul>"
     },
     2: {
         label: 'Modéré',
-        text: "Incident gérable avec des efforts supplémentaires ; impacts financiers ou opérationnels contenus."
+        text: "<p><strong>Impact financier</strong></p><ul><li>&lt; 3M&euro;</li></ul><p><strong>Impact juridique / r&eacute;glementaire</strong></p><ul><li>Proc&eacute;dure judiciaire ou administrative &agrave; l&rsquo;&eacute;chelle d&rsquo;un collaborateur</li></ul><p><strong>Impact r&eacute;putationnel</strong></p><ul><li><p>Impact externe r&eacute;gional (ex.&nbsp;: ARS)</p></li><li><p>Atteinte limit&eacute;e &agrave; quelques semaines</p></li></ul><p><strong>Impact op&eacute;rationnel</strong></p><ul><li><p>Perturbations l&eacute;g&egrave;res</p></li><li><p>Perte temporaire d&rsquo;activit&eacute;s ou de march&eacute;s</p></li></ul>"
     },
     3: {
         label: 'Fort',
-        text: "Perturbation significative de l'activité ou des relations externes avec exposition médiatique possible."
+        text: "<p><strong>Impact financier</strong></p><ul><li>&lt; 30M&euro;</li></ul><p><strong>Impact juridique / r&eacute;glementaire</strong></p><ul><li>Sanctions &agrave; l&rsquo;&eacute;chelle d&rsquo;une filiale Convention juridique d&rsquo;int&eacute;r&ecirc;t public (CJIP)</li></ul><p><strong>Impact r&eacute;putationnel</strong></p><ul><li><p>Impact externe national (ex.&nbsp;: minist&egrave;re de la Sant&eacute;)</p></li><li><p>Crise m&eacute;diatique nationale</p></li><li><p>Atteinte prolong&eacute;e sur plusieurs mois</p></li></ul><p><strong>Impact op&eacute;rationnel</strong></p><ul><li><p>Perturbations importantes</p></li><li><p>Perte d&eacute;finitive d&rsquo;activit&eacute;s ou de march&eacute;s</p></li></ul>"
     },
     4: {
         label: 'Critique',
-        text: "Atteinte grave à la continuité, sanctions réglementaires majeures ou dommages durables à la réputation."
+        text: "<p><strong>Impact financier</strong></p><ul><li>&ge; 30M&euro;</li></ul><p><strong>Impact juridique / r&eacute;glementaire</strong></p><ul><li><p>Sanctions &agrave; l&rsquo;&eacute;chelle du Groupe</p></li><li><p>Condamnation p&eacute;nale</p></li></ul><p><strong>Impact r&eacute;putationnel</strong></p><ul><li><p>Impact externe international (ex.&nbsp;: EMA, FDA, etc.)</p></li><li><p>Crise m&eacute;diatique internationale</p></li><li><p>Atteinte durable sur plusieurs ann&eacute;es</p></li></ul><p><strong>Impact op&eacute;rationnel</strong></p><ul><li><p>Arr&ecirc;t des activit&eacute;s</p></li></ul>"
     }
 };
 
